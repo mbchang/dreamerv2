117e8c2d78affcd0e2c697530d456e2fe895fe0c
Michael Chang <mbchang2017@gmail.com>
2021-10-16 10:21:36 -0700

    clean up

--- dreamerv2/sandbox/causal_agent.py
+++ dreamerv2/sandbox/causal_agent.py
@@ -99,7 +99,6 @@ class WorldModel(common.Module):
 
     if config.decoder_type == 'default':
       self.heads['decoder'] = machine.Decoder(shapes, **config.decoder)
-    # elif config.decoder_type == 'slot':
     elif config.decoder_type in ['slot', 'slimmerslot']:
       decoder_in_dim = config.rssm.deter + config.rssm.stoch * config.rssm.discrete
       self.heads['decoder'] = machine.SlotDecoder(shapes, decoder_in_dim, config.decoder_type, **config.decoder)
