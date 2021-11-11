ab5a2a5138a55a7a031442326f136988d7b5a5d9
Michael Chang <mbchang2017@gmail.com>
2021-11-10 22:10:07 -0800

    encapsulated slot_model

--- slate.py
+++ slate.py
@@ -7,15 +7,13 @@ from einops import rearrange
 import tensorflow as tf
 import tensorflow.keras.layers as layers
 
-class SLATE(layers.Layer):
+class SlotModel(layers.Layer):
     def __init__(self, args):
         super().__init__()
 
         self.num_slots = args.num_slots
         self.vocab_size = args.vocab_size
         self.d_model = args.d_model
-
-        self.dvae = dVAE(args.vocab_size, args.img_channels)
 
         self.positional_encoder = PositionalEncoding(1 + (args.image_size // 4) ** 2, args.d_model, args.dropout)
 
@@ -32,15 +30,109 @@ class SLATE(layers.Layer):
 
         self.out = linear(args.d_model, args.vocab_size, bias=False)
 
+        self.training = False
+
+    def embed_tokens(self, tokens):
+        emb_input = self.dictionary(tokens)
+        # emb_input = self.positional_encoder(emb_input)
+        emb_input = self.positional_encoder(emb_input, training=self.training)
+        return emb_input
+
+    def apply_slot_attn(self, emb_input):
+        slots, attns = self.slot_attn(emb_input[:, 1:])
+        slots = self.slot_proj(slots)
+        return slots, attns
+
+    def parallel_decode(self, emb_input, slots):
+        decoder_output = self.tf_dec(emb_input[:, :-1], slots)
+        pred = self.out(decoder_output)
+        return pred
+
+    def autoregressive_decode(self, z_hard, slots, gen_len):
+        B = slots.shape[0]
+
+        # generate image tokens auto-regressively
+        z_gen = z_hard.new_zeros(0)
+        z_transformer_input = z_hard.new_zeros(B, 1, self.vocab_size + 1)
+        z_transformer_input[..., 0] = 1.0
+        for t in range(gen_len):
+            decoder_output = self.tf_dec(
+                self.positional_encoder(self.dictionary(z_transformer_input)),
+                slots
+            )
+            z_next = F.one_hot(self.out(decoder_output)[:, -1:].argmax(dim=-1), self.vocab_size)
+            z_gen = torch.cat((z_gen, z_next), dim=1)
+            z_transformer_input = torch.cat([
+                z_transformer_input,
+                torch.cat([torch.zeros_like(z_next[:, :, :1]), z_next], dim=-1)
+            ], dim=1)
+
+        return z_gen
+
+    def call(self, z_transformer_input, z_transformer_target):
+        B = z_transformer_input.shape[0]
+        emb_input = self.embed_tokens(z_transformer_input)
+        slots, attns = self.apply_slot_attn(emb_input)
+        pred = self.parallel_decode(emb_input, slots)
+        # cross_entropy = -(z_transformer_target * torch.log_softmax(pred, dim=-1)).flatten(start_dim=1).sum(-1).mean()
+        cross_entropy = -tf.reduce_mean(tf.reduce_sum(tf.reshape(z_transformer_target * tf.nn.log_softmax(pred, axis=-1), (B, -1)), axis=-1))
+        return attns, cross_entropy
+
+
+    def train(self):
+        self.training = True
+
+    def eval(self):
+        self.training = False
+
+
+
+def create_tokens(z_hard):
+    # target tokens for transformer
+    z_transformer_target = rearrange(z_hard, 'b c h w -> b (h w) c')
+
+    # add BOS token
+    # z_transformer_input = torch.cat([torch.zeros_like(z_transformer_target[..., :1]), z_transformer_target], dim=-1)
+    # z_transformer_input = torch.cat([torch.zeros_like(z_transformer_input[..., :1, :]), z_transformer_input], dim=-2)
+    # z_transformer_input[:, 0, 0] = 1.0
+    B, zhw, zc = z_transformer_target.shape
+    z_transformer_input = tf.concat([tf.zeros((B, zhw, 1)), z_transformer_target], axis=-1)
+    z_transformer_input = tf.concat([
+        tf.concat([tf.ones((B, 1, 1)), tf.zeros((B, 1, zc))], axis=-1),
+         z_transformer_input], axis=-2)
+    return z_transformer_input, z_transformer_target
+
+
+class SLATE(layers.Layer):
+    def __init__(self, args):
+        super().__init__()
+
+        self.num_slots = args.num_slots
+        self.vocab_size = args.vocab_size
+        self.d_model = args.d_model
+
+        self.dvae = dVAE(args.vocab_size, args.img_channels)
+        self.slot_model = SlotModel(args)
+
         self.not_dvae = [
-            self.positional_encoder, 
-            self.slot_attn, 
-            self.dictionary, 
-            self.slot_proj, 
-            self.tf_dec, 
-            self.out]
+            self.slot_model.positional_encoder, 
+            self.slot_model.slot_attn, 
+            self.slot_model.dictionary, 
+            self.slot_model.slot_proj, 
+            self.slot_model.tf_dec, 
+            self.slot_model.out]
 
         self.training = False
+
+    def overlay_attention(self, attns, image, H_enc, W_enc):
+        B, C, H, W = image.shape
+        # attns = attns.transpose(-1, -2)
+        attns = rearrange(attns, 'b hw k -> b k hw')
+        # attns = attns.reshape(B, self.num_slots, 1, H_enc, W_enc).repeat_interleave(H // H_enc, dim=-2).repeat_interleave(W // W_enc, dim=-1)
+        attns = tf.repeat(tf.repeat(tf.reshape(attns, (B, self.num_slots, 1, H_enc, W_enc)), H // H_enc, axis=-2), W // W_enc, axis=-1)
+        # attns = image.unsqueeze(1) * attns + 1. - attns
+        attns = rearrange(image, 'b c h w -> b 1 c h w') * attns + 1. - attns
+        return attns
 
     def call(self, image, tau, hard):
         """
@@ -52,40 +144,10 @@ class SLATE(layers.Layer):
         recon, z_hard, mse = self.dvae(image, tau, hard)
         _, _, H_enc, W_enc = z_hard.shape
 
-        # target tokens for transformer
-        z_transformer_target = rearrange(z_hard, 'b c h w -> b (h w) c')
+        z_transformer_input, z_transformer_target = create_tokens(z_hard)
+        attns, cross_entropy = self.slot_model(z_transformer_input, z_transformer_target)
 
-        # add BOS token
-        # z_transformer_input = torch.cat([torch.zeros_like(z_transformer_target[..., :1]), z_transformer_target], dim=-1)
-        # z_transformer_input = torch.cat([torch.zeros_like(z_transformer_input[..., :1, :]), z_transformer_input], dim=-2)
-        # z_transformer_input[:, 0, 0] = 1.0
-        _, zhw, zc = z_transformer_target.shape
-        z_transformer_input = tf.concat([tf.zeros((B, zhw, 1)), z_transformer_target], axis=-1)
-        z_transformer_input = tf.concat([
-            tf.concat([tf.ones((B, 1, 1)), tf.zeros((B, 1, zc))], axis=-1),
-             z_transformer_input], axis=-2)
-
-        # tokens to embeddings
-        emb_input = self.dictionary(z_transformer_input)
-        # emb_input = self.positional_encoder(emb_input)
-        emb_input = self.positional_encoder(emb_input, training=self.training)
-
-        # apply slot attention
-        slots, attns = self.slot_attn(emb_input[:, 1:])
-
-        # attns = attns.transpose(-1, -2)
-        attns = rearrange(attns, 'b hw k -> b k hw')
-        # attns = attns.reshape(B, self.num_slots, 1, H_enc, W_enc).repeat_interleave(H // H_enc, dim=-2).repeat_interleave(W // W_enc, dim=-1)
-        attns = tf.repeat(tf.repeat(tf.reshape(attns, (B, self.num_slots, 1, H_enc, W_enc)), H // H_enc, axis=-2), W // W_enc, axis=-1)
-        # attns = image.unsqueeze(1) * attns + 1. - attns
-        attns = rearrange(image, 'b c h w -> b 1 c h w') * attns + 1. - attns
-
-        # apply transformer
-        slots = self.slot_proj(slots)
-        decoder_output = self.tf_dec(emb_input[:, :-1], slots)
-        pred = self.out(decoder_output)
-        # cross_entropy = -(z_transformer_target * torch.log_softmax(pred, dim=-1)).flatten(start_dim=1).sum(-1).mean()
-        cross_entropy = -tf.reduce_mean(tf.reduce_sum(tf.reshape(z_transformer_target * tf.nn.log_softmax(pred, axis=-1), (B, -1)), axis=-1))
+        attns = self.overlay_attention(attns, image, H_enc, W_enc)
 
         return (
             # recon.clamp(0., 1.),
@@ -155,9 +217,11 @@ class SLATE(layers.Layer):
 
     def train(self):
         self.training = True
+        self.slot_model.train()
 
     def eval(self):
         self.training = False
+        self.slot_model.eval()
 
 
 class OneHotDictionary(layers.Layer):
